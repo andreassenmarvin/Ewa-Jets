109e0dc9d7f56ff67b3deb2af8ae697d6e019f99
Marvin Macharia <machariamarvin625@gmail.com>
2022-08-10 12:40:53 +0300

    Create navbar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Header
+
 var myVideo = document.getElementById("header");
 
 myVideo.addEventListener("ended", function () {
@@ -33,3 +35,24 @@ function typing() {
         $(explorebtn).slideDown();
     }
 }
+
+// Navbar
+
+$(function () {
+    $("#navbar-toggler-icon").click(function () {
+        $("#navbarSupportedContent").slideToggle();
+        $(".form").css({
+            "margin": "auto"
+        })
+    })
+
+    $("#privateDropdown").click(function () {
+        $("#dropdown1").slideToggle();
+        $("#dropdown2").slideUp();
+    })
+
+    $("#airDropdown").click(function () {
+        $("#dropdown2").slideToggle();
+        $("#dropdown1").slideUp();
+    })
+})
